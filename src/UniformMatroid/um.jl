9838771da32ff72beed671f60430d5c73e78bed2
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-20 03:47:04 +0200

    Add types for uniform matroid.

--- src/UniformMatroid/um.jl
+++ src/UniformMatroid/um.jl
@@ -1,18 +1,20 @@
 """
-An instance of the m-set problem.
+An instance of the uniform-matroid problem. It is sometimes called the m-set
+problem. 
 
-Values are not restricted.
+Any set of `m` values or fewer is a solution. More formally, the independent 
+sets are the sets containing at most `m` elements.
 
 It can be formalised as follows:
 
 ``\\max \\sum_i \\mathrm{values}_i x_i``
 ``\\mathrm{s.t.} \\sum_i x_i \\leq m, \\quad x \\in \\{0, 1\\}^d``
 """
-struct MSetInstance <: CombinatorialInstance
-  values::Vector{Float64}
+struct MSetInstance{T <: Real} <: CombinatorialInstance
+  values::Vector{T}
   m::Int
 
-  function MSetInstance(values::Vector{Float64}, m::Int)
+  function MSetInstance(values::Vector{T}, m::Int) where {T <: Real}
     # Error checking.
     if m < 0
       error("m is less than zero: there is no solution.")
@@ -23,22 +25,22 @@ struct MSetInstance <: CombinatorialInstance
     end
 
     # Return a new instance.
-    new(values, m)
+    return new(values, m)
   end
 end
 
-values(i::MSetInstance) = i.values
+values(i::MSetInstance{T}) where {T} = i.values
 m(i::MSetInstance) = i.m
-
-value(i::MSetInstance, o::Int) = values(i)[o]
-values(i::MSetInstance, o) = values(i)[o]
 dimension(i::MSetInstance) = length(values(i))
 
-struct MSetSolution <: CombinatorialSolution
-  instance::MSetInstance
+value(i::MSetInstance{T}, o::Int) where {T} = values(i)[o]
+values(i::MSetInstance{T}, o) where {T} = values(i)[o]
+
+struct MSetSolution{T <: Real} <: CombinatorialSolution
+  instance::MSetInstance{T}
   items::Vector{Int} # Indices to the chosen items.
 end
 
-function value(s::MSetSolution)
+function value(s::MSetSolution{T}) where {T <: Real}
   return sum(s.instance.values[i] for i in s.items)
 end
