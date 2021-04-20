dc3ea2a3c494fac75d8b7bae13510e4d2db381fc
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-20 04:04:40 +0200

    Missing documentation.

--- src/UniformMatroid/um_lp.jl
+++ src/UniformMatroid/um_lp.jl
@@ -1,12 +1,12 @@
 function solve(i::UniformMatroidInstance, ::DefaultLinearFormulation; solver=nothing)
-  model = Model(solver)
-  
-  @variable(model, x[1:length(values(i))], Bin)
-  @objective(model, Max, dot(x, values(i)))
-  @constraint(model, sum(x) <= m(i))
+    model = Model(solver)
+    
+    @variable(model, x[1:length(values(i))], Bin)
+    @objective(model, Max, dot(x, values(i)))
+    @constraint(model, sum(x) <= m(i))
 
-  set_silent(model)
-  optimize!(model)
+    set_silent(model)
+    optimize!(model)
 
-  return UniformMatroidSolution(i, findall(JuMP.value.(x) .>= 0.5))
+    return UniformMatroidSolution(i, findall(JuMP.value.(x) .>= 0.5))
 end
--- src/interface/variants.jl
+++ src/interface/variants.jl
@@ -1,5 +1,10 @@
 """
+    struct MinimumBudget{CI <: CombinatorialInstance, T <: Real}
 
+Implement a minimum-budget constraint, i.e. a minimum quantity of a resource 
+that must be used.
+
+``\\sum_i x_i \\times \\mathtt{weights}_i \\geq \\mathtt{min\\_budget}``
 """
 struct MinimumBudget{CI <: CombinatorialInstance, T <: Real} <: CombinatorialVariation
     instance::CI
@@ -7,8 +12,16 @@ struct MinimumBudget{CI <: CombinatorialInstance, T <: Real} <: CombinatorialVar
     min_budget::T
 end
 
+"""
+    struct MaximumBudget{CI <: CombinatorialInstance, T <: Real}
+
+Implement a maximum-budget constraint, i.e. a maximum quantity of a resource 
+that can be used.
+
+``\\sum_i x_i \\times \\mathtt{weights}_i \\leq \\mathtt{max\\_budget}``
+"""
 struct MaximumBudget{CI <: CombinatorialInstance, T <: Real} <: CombinatorialVariation
     instance::CI
     weights::Vector{T}
-    min_budget::T
+    max_budget::T
 end
