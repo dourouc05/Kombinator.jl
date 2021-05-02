91d51af3ff152406b963cbd4352dd6f685f7b61d
Thibaut Cuvelier <dourouc05@gmail.com>
2021-05-02 17:34:51 +0200

    Document solve.

--- src/interface/instance.jl
+++ src/interface/instance.jl
@@ -67,6 +67,18 @@ that contains all the required information for the supplementary constraints
 abstract type CombinatorialVariation <: CombinatorialInstance end
 
 """
+    solve(i::CombinatorialInstance, algo::CombinatorialAlgorithm)
+
+Solve the given combinatorial instance `i` using the provided algorithm
+`algo` (potentially containing parameters for the solving process). The 
+returned value is a `CombinatorialSolution`. 
+
+If the requested algorithm is not available for this type of instance, 
+a `MethodError` is thrown.
+"""
+function solve end
+
+"""
     objective(i::CombinatorialInstance)
 
 Returns the objective associated with this instance. 
