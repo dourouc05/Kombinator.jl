2f9cd77d5ff3b3016913d99f278abe83789c6ca4
Thibaut Cuvelier <dourouc05@gmail.com>
2021-05-02 21:43:47 +0200

    Script for EP experiments.

--- experiments/runexperiments.jl
+++ experiments/runexperiments.jl
@@ -8,7 +8,7 @@ n_repetitions = 10
 
 run_um = true
 run_st = true
-run_ep = false
+run_ep = true
 
 # Generic helpers.
 function parse_algo_string(str::String)
@@ -126,4 +126,48 @@ end
 
 # Elementary path.
 if run_ep
+    ep = DataFrame(method=String[], algo=String[], name=String[], nonlinearity=String[], size=Int[], repetition=Int[], objective=Float64[], time_ms=Float64[])
+    ep_sizes = [2, 5, 10, 20, 50, 100, 200, 500]
+    ep_algos = ["Exact", "Approx: DP", "Approx: LP"]
+
+    for repetition in 1:n_repetitions
+        for size in ep_sizes
+            g0 = complete_graph(size) # Only for determining the edges
+            g = complete_digraph(size)
+            hi = 1.0
+            lo = 0.5
+
+            lw = Dict{Edge{Int}, Float64}(e => ifelse(i <= size / 2, hi, lo) for (i, e) in enumerate(edges(g0)))
+            nlw = Dict{Edge{Int}, Float64}(e => ifelse(i <= size / 2, lo, hi) for (i, e) in enumerate(edges(g0)))
+
+            for ep_algo in ep_algos
+                method, algo, nls = parse_algo_string(ep_algo)
+
+                li = ElementaryPathInstance(g, lw, 1, size, Maximise())
+                nli = NonlinearCombinatorialInstance(
+                    li,
+                    lw,
+                    nlw,
+                    SquareRoot,
+                    0.5,
+                    DefaultLinearFormulation(Gurobi.Optimizer),
+                    true,
+                    DefaultLinearFormulation(Gurobi.Optimizer),
+                )
+
+                t0 = time_ns()
+                s = solve(nli, nls)
+                t1 = time_ns()
+
+                time_ms = (t1 - t0) / 1_000_000
+                obj_lin = sum(e in keys(lw) ? lw[e] : lw[reverse(e)] for e in s.variables)
+                obj_nl = sum(e in keys(nlw) ? nlw[e] : nlw[reverse(e)] for e in s.variables)
+                obj = obj_lin + sqrt(obj_nl)
+
+                push!(ep, Dict(:method => method, :algo => algo, :name => ep_algo, :nonlinearity => "Square root", :size => size, :repetition => repetition, :objective => obj, :time_ms => time_ms))
+            end
+        end
+    end
+
+    CSV.write(@__DIR__() * "/ep.csv", ep)
 end
